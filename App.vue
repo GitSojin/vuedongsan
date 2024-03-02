a0e59231306297a4ed3faf24a446c95c3f667aec
GitSojin <121475051+GitSojin@users.noreply.github.com>
2024-03-02 19:23:01 +0900

    뷰동산2

--- App.vue
+++ App.vue
@@ -1,8 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div v-for="(product,index) in products" :key="index">
-    <h4 class="blue" :style=color>{{ product }} 원룸</h4>
-    <p>{{ price1 }} 만원</p>
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+  </div>
+  <div v-for="(item, index) in products" :key="index">
+    <h4 class="blue" :style="color">{{ item.product }} 원룸</h4>
+    <p>{{ item.price }} 만원</p>
   </div>
 </template>
 
@@ -12,9 +14,13 @@ export default {
   components: {},
   data() {
     return {
-      price1: 50,
       스타일: "color:blue",
-      products : ['역삼동원룸','천호동원룸','마포구원룸']
+      메뉴들: ["Home", "Shop", "About"],
+      products: [
+        { product: "역삼동원룸", price: 50 },
+        { product: "천호동원룸", price: 60 },
+        { product: "마포구원룸", price: 70 },
+      ],
     };
   },
 };
@@ -27,6 +33,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
